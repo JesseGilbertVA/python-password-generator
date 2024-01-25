037c142cf7b6ad58a17c29d8e66fcc07a2b9fdee
Jesse Gilbert <jessegilbertva@gmail.com>
2024-01-25 14:51:23 -0700

    Updated to allow more specific criteria for password generation.

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Python Password Generator
 
-Still a work in progress.
-
-Simple Python terminal password generator. Generates password length based on user input. Eventually will allow specific criteria to be set by user to customize password. May also eventually add GUI.
+Simple Python terminal password generator. Generates password based on user input. May also eventually add GUI.
--- generator.py
+++ generator.py
@@ -1,21 +1,95 @@
 import random
 import string
 
-def passwordLength():
-    print('Enter desired strong password length: ')
-    userInput = int(input())
-    return userInput
+class invalidInteger(Exception):
+    pass
 
-def generate_password(length):
-    characters = string.ascii_letters + string.digits + string.punctuation
+def getLetterCriteria():
+    print('Would you like your password to have uppercase letters, lowercase letters, both, or none?')
+    letterCasing = int(input('1. Uppercase only \n2. Lowercase only \n3. Both uppercase and lowercase \n4. No letters \n'))
+    if letterCasing >= 5 :
+        raise invalidInteger
+    elif letterCasing <= 0:
+        raise invalidInteger
+    return letterCasing
+
+def getNumberCriteria():
+    print('Would you like your password to contain numbers?')
+    containNumbers = int(input('1. Yes \n2. No\n'))
+    if containNumbers >= 3:
+        raise invalidInteger
+    elif containNumbers <= 0:
+        raise invalidInteger
+    return containNumbers
+
+def getSpecialCriteria():
+    print('Would you like your password to contain special characters?')
+    containSpecial = int(input('1. Yes \n2. No \n'))
+    if containSpecial >= 3:
+        raise invalidInteger
+    elif containSpecial <= 0:
+        raise invalidInteger
+    return containSpecial
+
+def passwordLength():
+    print('Lastly, please enter desired password length: ')
+    lengthInput = int(input())
+    if lengthInput <= 0:
+        raise invalidInteger
+    return lengthInput
+
+def generatePassword(length):
+    if letterCharactersInput == 1:
+        letterCharacters = string.ascii_uppercase
+    elif letterCharactersInput == 2:
+        letterCharacters = string.ascii_lowercase
+    elif letterCharactersInput == 3:
+        letterCharacters = string.ascii_letters
+    else:
+        letterCharacters = ''
+    if numberCharactersInput == 1:
+        numberCharacters = string.digits
+    else:
+        numberCharacters = ''
+    if specialCharactersInput == 1:
+        specialCharacters = string.punctuation
+    else:
+        specialCharacters = ''
+    if letterCharactersInput == 4 and numberCharactersInput == 2 and specialCharactersInput == 2:
+        print('No password criteria specified. Terminating.')
+        exit(1)
+
+    characters = letterCharacters + numberCharacters + specialCharacters
     password = ''.join(random.choice(characters) for i in range(length))
     print('Your secure password is: ' + password)
 
+print('Welcome to the strong password generator.')
 while True:
     try:
-        inputLength = passwordLength()
+        letterCharactersInput = getLetterCriteria()
     except:
-        print('Invalid input. Please enter an integer.')
+        print('Invalid input. Please enter a valid integer.')
     else: break
 
-generate_password(inputLength)
+while True:
+    try:
+        numberCharactersInput = getNumberCriteria()
+    except:
+        print('Invalid input. Please enter a valid integer.')
+    else: break
+
+while True:
+    try:
+        specialCharactersInput = getSpecialCriteria()
+    except:
+        print('Invalid input. Please enter a valid integer.')
+    else: break
+
+while True:
+    try:
+        totalChractersInput = passwordLength()
+    except:
+        print('Invalid input. Please enter a valid integer.')
+    else: break
+
+generatePassword(totalChractersInput)
